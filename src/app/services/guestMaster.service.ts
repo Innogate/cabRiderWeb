308a416a2e9929bae07bbf77a435b1c9ebbaa31b
Amit maity <amitmaity2024@gmail.com>
2025-07-13 16:36:10 +0530

    create guest master service

---
+++ src/app/services/guestMaster.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import { WebSocketService } from './websocket.service';
+import { BaseService } from './base.service';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class guestMasterService extends BaseService {
+  constructor(ws: WebSocketService) {
+    super(ws);
+  }
+
+  GetAllGuest(payload: any) {
+    this.post('/master/gatAllGuestMaster', payload);
+  }
+
+//   CreateUpdateParty(payload: any){
+//     this.post('/master/createUpdatePart', payload);
+//   }
+
+//   Delete(payload: any){
+//     this.post('/master/deleteCharges', payload);
+//   }
+
+  protected handleMessage(msg: any): boolean {
+    return false;
+  }
+}
