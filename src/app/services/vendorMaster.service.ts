d693ec90adcd329c6d0c792ec19cf47bd528695a
Amit maity <amitmaity2024@gmail.com>
2025-07-13 16:07:30 +0530

    create service vendor master

---
+++ src/app/services/vendorMaster.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import { WebSocketService } from './websocket.service';
+import { BaseService } from './base.service';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class vendorMasterService extends BaseService {
+  constructor(ws: WebSocketService) {
+    super(ws);
+  }
+
+  GatAllVendor(payload: any) {
+    this.post('/master/gatAllVendorMaster', payload);
+  }
+
+//   CreateUpdateParty(payload: any){
+//     this.post('/master/createUpdatePart', payload);
+//   }
+
+//   Delete(payload: any){
+//     this.post('/master/deleteCharges', payload);
+//   }
+
+  protected handleMessage(msg: any): boolean {
+    return false;
+  }
+}
