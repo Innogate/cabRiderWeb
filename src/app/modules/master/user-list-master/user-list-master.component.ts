35bc20de7d79ade41bce281bb5517d2ca5360c8f
SAGAR-21-max <sagarchakraborty9800@gmail.com>
2025-07-20 21:38:42 +0530

    genrate component user master

---
+++ src/app/modules/master/user-list-master/user-list-master.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-list-master',
+  imports: [],
+  templateUrl: './user-list-master.component.html',
+  styleUrl: './user-list-master.component.css'
+})
+export class UserListMasterComponent {
+
+}
