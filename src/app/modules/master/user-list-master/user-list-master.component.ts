afd649ab5b5a1359a1233efaeed9a8419b172b79
SAGAR-21-max <sagarchakraborty9800@gmail.com>
2025-07-24 21:33:29 +0530

    Create UI User list

--- src/app/modules/master/user-list-master/user-list-master.component.ts
+++ src/app/modules/master/user-list-master/user-list-master.component.ts
@@ -8,7 +8,8 @@ import { globalRequestHandler } from '../../../utils/global';
 import { DynamicTableComponent } from '../../../components/dynamic-table/dynamic-table.component';
 import { DropdownModule } from 'primeng/dropdown';
 import { AutoCompleteModule } from 'primeng/autocomplete';
-import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { commonService } from '../../../services/comonApi.service';
 
 @Component({
   selector: 'app-user-list-master',
@@ -22,44 +23,49 @@ export class UserListMasterComponent implements OnInit, OnDestroy, AfterViewInit
   form!: FormGroup;
   heading: String = '';
   showForm: boolean = false;
-
+  tablevalue: any;
 
     constructor(
     private userListMasterService:userMasterService, 
     private router:Router,
     private messageService:MessageService,
     private fb: FormBuilder,
+    private commonService: commonService
     ){
       this.createForm();
     }
   createForm() {
     this.form = this.fb.group({
       id: [0],
-      party_name: [''],
-      address: [''],
-      city_id: [],
-      pin_code: [''],
-      mobileno: [''],
-      whatsappno: [''],
-      gstno: [''],
-      panno: [''],
-      refby: [''],
-      active: ['Y'],
-      crdays: [0],
-      crlimit: [0],
-      email: [''],
-      CGST: [0],
-      SGST: [0],
-      IGST: [0],
-      phone_no: [''],
-      credit_days: [0],
-      credit_limit: [0],
-      tax_type: ['CGST/SGST'],
-      rcm: ['N'],
-      cinno: [''],
-      msmeno: [''],
-      billing_instruction: [''],
-      alias: [''],
+      Name: 
+          ['', [Validators.required, Validators.pattern(/^[A-Za-z ]+$/)]],
+      email: 
+          ['',[Validators.required,Validators.email,Validators.maxLength(100)]],
+      mobile:
+          ['',
+            [
+              Validators.required,
+              Validators.pattern(/^[6-9]\d{9}$/), // Indian mobile number format
+            ],
+          ],
+      password:
+          ['',
+            [
+              Validators.required,
+              Validators.minLength(6),
+              Validators.maxLength(20),
+              Validators.pattern(/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d@$!%*?&]+$/), // at least 1 letter, 1 number
+            ],
+          ],
+      username:
+          ['',
+            [
+              Validators.required,
+              Validators.minLength(4),
+              Validators.maxLength(30),
+              Validators.pattern(/^[a-zA-Z0-9_.-]+$/), // allow alphanum, underscore, dot, dash
+            ],
+          ],
     });
   }
 ngOnInit(): void {
@@ -70,7 +76,38 @@ ngOnInit(): void {
         this.isLoading = false;
         this.data = msg.data;
       }
+      else if(msg.for === 'createUpdateUser'){
+        if (msg.StatusID === 1) {
+          const updated = msg.data[0];  // access the first item in data array
+
+          this.messageService.add({ severity: 'success', summary: 'Success', detail: msg.StatusMessage });
+          this.showForm = false;
+          this.form.reset();
+
+          const index = this.data.findIndex((v: any) => v.id == updated.id);
+          if (index !== -1) {
+            this.data[index] = { ...updated };
+          } else {
+            this.data.push(updated)
+          }
+        } else {
+          this.messageService.add({ severity: 'error', summary: 'Error', detail: msg.StatusMessage });
+        }
+
+      }
+      else if (msg.for === "deleteData") {
+        if (msg.StatusMessage === "success") {
+          const index = this.data.findIndex((v: any) => v.id == this.tablevalue.id);
+          if (index !== -1) {
+            this.data.splice(index, 1);
+          } 
+          this.messageService.add({ severity: 'success', summary: 'Success', detail: msg.StatusMessage })
+        } else {
+          this.messageService.add({ severity: 'error', summary: 'Error', detail: "Cannot Delete data" })
+        }
+      }
       return true;
+
     });
   }
 
@@ -114,15 +151,35 @@ ngOnInit(): void {
       case 'edit':
         this.heading = 'UPDATE USER LIST';
         this.showForm = true;
-        console.log("Edit");
+        this.form.patchValue(event.data)
         break;
       case 'delete':
+      this.deleteUser(event.data)
+      this.tablevalue =event.data
         break;
       case 'add':
         this.heading = 'ADD USER LIST';
         this.showForm = !this.showForm;
-        console.log("Add");
+
         break
     }
   }
+  saveUser(){
+   if(this.form.invalid){
+      this.messageService.add({ severity: 'Error', summary: 'Error', detail: 'Please Input valid Data' });
+      return;
+   }
+   this.userListMasterService.createUpdateUser(this.form.value)
+
+  }
+   private deleteUser(user: any) {
+    this.messageService.add({ severity: 'contrast', summary: 'Info', detail: 'Please wait processing...' });
+    const payload = {
+      table_name: "tbl_users",
+      column_name: "id",
+      column_value: "" + user.id,
+    }
+    this.commonService.deleteData(payload)
+  }
+
 }
