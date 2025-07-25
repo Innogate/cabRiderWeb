5a364722a317c9797a5625960989848c81d427d6
madhumita <madhumitarana554@gmail.com>
2025-07-25 19:58:20 +0530

    invoice rcm yes

--- src/app/modules/entry/invoice-add/invoice-add.component.ts
+++ src/app/modules/entry/invoice-add/invoice-add.component.ts
@@ -18,10 +18,17 @@ import { RippleModule } from 'primeng/ripple';
 import { TableModule } from 'primeng/table';
 import { TagModule } from 'primeng/tag';
 import { TooltipModule } from 'primeng/tooltip';
+import { globalRequestHandler } from '../../../utils/global';
+import { carTypeMasterService } from '../../../services/carTypeMaster.service';
+import { Router } from '@angular/router';
+import { MessageService } from 'primeng/api';
+import { commonService } from '../../../services/comonApi.service';
+import { AutoComplete } from 'primeng/autocomplete';
 
 @Component({
   selector: 'app-invoice-add',
   imports: [
+    AutoComplete,
     FormsModule,
     RadioButtonModule,
     TableModule,
@@ -45,6 +52,7 @@ export class InvoiceAddComponent {
   taxType = 'cgst';
   rcm = 'no';
   billDate = new Date();
+  carTypes?: any[];
 
   searchText: any;
   selectedShow: any;
@@ -53,13 +61,17 @@ export class InvoiceAddComponent {
 
   invoiceForm!: FormGroup;
 
+  charges = [
+    { name: 'Fuel Surcharge', amount: 200 },
+    { name: 'Driver Allowance', amount: 100 },
+  ];
+
   companies = [{ label: 'ABC Ltd', value: 1 }];
   branches = [{ label: 'Main Branch', value: 101 }];
   parties = [{ label: 'XYZ Pvt Ltd', value: 501 }];
   cities = [{ label: 'Delhi', value: 301 }];
 
-
- invoices = [
+  invoices = [
     {
       selected: false,
       siNo: 'DguGFIJKK',
@@ -79,53 +91,150 @@ export class InvoiceAddComponent {
       totalKm: 200,
       netAmount: 1500,
 
-// id
-// City
-// duty_type
-// branch_id
-// company_id
-// branch
-// party_id
-// city_id
-// BillNo
-// BillDate
-// taxtype
-// rcm
-// GrossAmount
-// OtherCharges
-// Discount
-// CGSTPer
-// CGST
-// SGSTPer
-// SGST
-// OtherCharges2
-// RoundOff
-// NetAmount
-// Advance
+      // id
+      // City
+      // duty_type
+      // branch_id
+      // company_id
+      // branch
+      // party_id
+      // city_id
+      // BillNo
+      // BillDate
+      // taxtype
+      // rcm
+      // GrossAmount
+      // OtherCharges
+      // Discount
+      // CGSTPer
+      // CGST
+      // SGSTPer
+      // SGST
+      // OtherCharges2
+      // RoundOff
+      // NetAmount
+      // Advance
     },
     // Add more invoice entries here...
   ];
 
-
-  constructor(private fb: FormBuilder) {}
-
-  ngOnInit() {
+  init() {
     this.invoiceForm = this.fb.group({
-      id:[null],
-      company_id: [null],
-      branch_id: [null],
-      party_id: [null],
-      City: [null],
+      id: [''],
+      City: [''],
+      duty_type: [''],
+      branch_id: [''],
+      company_id: [''],
+      branch: [''],
+      party_id: [''],
+      city_id: [''],
       BillNo: ['NEW'],
       BillDate: [new Date()],
       taxtype: ['cgst'],
       rcm: ['no'],
-      duty_type:[null],
-      branch:[null],
-      city_id:[null],
-
+      GrossAmount: ['0'],
+      OtherCharges: ['0'],
+      Discount: [''],
+      CGSTPer: [''],
+      CGST: ['0'],
+      SGSTPer: [''],
+      SGST: ['0'],
+      OtherCharges2: ['0'],
+      RoundOff: ['0'],
+      NetAmount: ['0'],
+      Advance: [''],
     });
   }
+
+  carTypeSearch: any;
+
+  constructor(
+    private fb: FormBuilder,
+    private carTypeMaster: carTypeMasterService,
+    private router: Router,
+    private messageService: MessageService,
+    private commonApiService: commonService
+  ) {}
+
+  ngOnInit(): void {
+    this.carTypeMaster.registerPageHandler((msg) => {
+      let rt = false;
+      rt = globalRequestHandler(msg, this.router, this.messageService);
+      if (msg.for) {
+        if (msg.for === 'CarTypeGate') {
+          this.carTypes = msg.data;
+          rt = true;
+        } else if (msg.for === 'getAllCityDropdown') {
+          this.cities = msg.data;
+          rt = true;
+        } else if (msg.for === 'getAllBranchDropdown') {
+          this.branches = msg.data;
+          rt = true;
+        } else if (msg.for === 'getAllPartyDropdown') {
+          this.PartyName = msg.data;
+          rt = true;
+        }
+      }
+      if (rt == false) {
+        console.log(msg);
+      }
+      return rt;
+    });
+
+    this.getAllCity();
+    this.getAllBranches();
+    this.getAllParty();
+    this.init();
+  }
+
+   // AutoComplete
+  PartyName: any[] = []; // original full list
+  filteredPartyName: any[] = []; // used by the autocomplete
+
+  filterPartyName(event: any) {
+    const query = event.query?.toLowerCase() || '';
+    this.filteredPartyName = this.PartyName.filter((party) => {
+      const name = party.party_name?.toLowerCase() || '';
+      return name.includes(query);
+    });
+  }
+
+  // API CALLS
+
+  getAllCity() {
+    this.commonApiService.GatAllCityDropDown({});
+  }
+
+  getAllBranches() {
+    this.commonApiService.GatAllBranchDropDown({});
+  }
+
+  getAllParty() {
+    this.commonApiService.gateAllPartyNameDropdown();
+  }
+
+  // OnSelect Functions
+  onBranchSelect(branch: any) {
+    if (this.invoiceForm) {
+      this.invoiceForm.get('branch_id')?.setValue(branch.value.Id);
+      console.log(branch);
+    }
+  }
+
+  onCitySelect(city: any) {
+    if (this.invoiceForm) {
+      this.invoiceForm.get('city_id')?.setValue(city.value.Id);
+    }
+    console.log(city);
+  }
+
+  onPartyNameSelect(party: any) {
+    if (this.invoiceForm) {
+      this.invoiceForm.get('party_id')?.setValue(party.value.id);
+    }
+    console.log(party);
+  }
+
 
   save() {
     const formData = {
@@ -139,8 +248,6 @@ export class InvoiceAddComponent {
   searchInvoices() {
     throw new Error('Method not implemented.');
   }
-
-
 
   addVendorInvoice() {
     // Logic to add invoice
@@ -157,4 +264,7 @@ export class InvoiceAddComponent {
   deleteInvoice(invoice: any) {
     // Logic to delete invoice
   }
+
+
+
 }
