3e763c78186458751c609f09aa56b96edb538ed2
tuhin-su <tuhinsubhraghara29195@gmail.com>
2025-08-02 00:58:45 +0530

    create files

---
+++ src/app/modules/entry/monthlyInvoice/monthly-invoice-list/monthly-invoice-list.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-monthly-invoice-list',
+  imports: [],
+  templateUrl: './monthly-invoice-list.component.html',
+  styleUrl: './monthly-invoice-list.component.css'
+})
+export class MonthlyInvoiceListComponent {
+
+}
