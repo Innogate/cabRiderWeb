33f58e2d2a7bd73e4a95754d12e721366c43ee46
SAGAR-21-max <sagarchakraborty9800@gmail.com>
2025-08-14 15:34:08 +0530

    Routing

---
+++ src/app/modules/master/branch-master/branch-master.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-branch-master',
+  imports: [],
+  templateUrl: './branch-master.component.html',
+  styleUrl: './branch-master.component.css'
+})
+export class BranchMasterComponent {
+
+}
