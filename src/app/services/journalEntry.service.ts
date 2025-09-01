de6e2b72ae55b357ac7be4a694bf461eb29cf259
Amit maity <amitmaity2024@gmail.com>
2025-09-01 23:21:30 +0530

    add service get journal by id

--- src/app/services/journalEntry.service.ts
+++ src/app/services/journalEntry.service.ts
@@ -11,48 +11,55 @@ export class JournalEntryService extends BaseService {
     super(ws);
   }
 
-  getJournal(payload: any){
+  getJournal(payload: any) {
     this.call('entry.getAllJournalHeaders', payload);
   }
 
-    //  "body": {
-    //     "page": 1,
-    //     "pageSize": 10
-    // }   
-  
-  createJournal(payload: any){
+  //  "body": {
+  //     "page": 1,
+  //     "pageSize": 10
+  // }   
+
+  createJournal(payload: any) {
     this.call('entry.createJournalEntry', payload);
   }
 
-//   {
-//   "seletedCompany": 1001,
-//   "branchId": 10,
-//   "vouchNo": "JV-2025-0001",
-//   "vouchDate": "2025-08-20T00:00:00",
-//   "narr": "Being payment made towards supplier invoice",
-//   "totalDebitAmt": 1000.00,
-//   "totalCreditAmt": 1000.00,
-//   "amtAdjusted": 0.00,
-//   "cancelYN": "N",
-//   "cancelBy": null,
-//   "cancelOn": null,
-//   "cancelReason": null,
-//   "user_id": 1,
-//    "transactions": [
-//     {
-//       "ledgerType": "DR",
-//       "partyId": 101,
-//       "debitAmt": 1000.00,
-//       "creditAmt": 0.00
-//     },
-//     {
-//       "ledgerType": "CR",
-//       "partyId": 202,
-//       "debitAmt": 0.00,
-//       "creditAmt": 1000.00
-//     }
-//   ]
-// }
+  //   {
+  //   "seletedCompany": 1001,
+  //   "branchId": 10,
+  //   "vouchNo": "JV-2025-0001",
+  //   "vouchDate": "2025-08-20T00:00:00",
+  //   "narr": "Being payment made towards supplier invoice",
+  //   "totalDebitAmt": 1000.00,
+  //   "totalCreditAmt": 1000.00,
+  //   "amtAdjusted": 0.00,
+  //   "cancelYN": "N",
+  //   "cancelBy": null,
+  //   "cancelOn": null,
+  //   "cancelReason": null,
+  //   "user_id": 1,
+  //    "transactions": [
+  //     {
+  //       "ledgerType": "DR",
+  //       "partyId": 101,
+  //       "debitAmt": 1000.00,
+  //       "creditAmt": 0.00
+  //     },
+  //     {
+  //       "ledgerType": "CR",
+  //       "partyId": 202,
+  //       "debitAmt": 0.00,
+  //       "creditAmt": 1000.00
+  //     }
+  //   ]
+  // }
+
+  getJournalById(payload: any) {
+    this.call('entry.getJournalById', payload);
+  }
+  // {
+  //   journal_id: 1
+  // }
 
 
   protected handleMessage(msg: any): boolean {
